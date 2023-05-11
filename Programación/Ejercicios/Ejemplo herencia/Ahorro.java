a9834fb8359e4cf0c8108e98c7649e7719758446
Smoothy <132601230+SmoothyCoronel@users.noreply.github.com>
2023-05-11 21:17:14 +0200

    Add files via upload

---
+++ Programación/Ejercicios/Ejemplo herencia/Ahorro.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuentabancaria;
+
+/**
+ *
+ * @author mint
+ */
+public class Ahorro extends CuentaBancaria{
+    private float interesAnual;
+    
+    public Ahorro(String dniTitular, String nombre, String email, String telefono, String iban) {
+        super(dniTitular, nombre, email, telefono, iban);
+        this.interesAnual = 0.1f;
+    }
+    
+    public void ajustarInteres(){
+        saldo -= interesAnual*saldo;
+    }
+}
---
+++ Programación/Ejercicios/Ejemplo herencia/Deposito.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuentabancaria;
+
+/**
+ *
+ * @author mint
+ */
+public class Deposito extends CuentaBancaria{
+    private float interesAnual;
+
+    public Deposito(String dniTitular, String nombre, String email, String telefono, String iban) {
+        super(dniTitular, nombre, email, telefono, iban);
+        this.interesAnual = 0.4f;
+    }
+
+    @Override
+    public boolean addOperacion(String fecha, String concepto, byte hora, float cantidad) {
+        if(fecha == null || fecha.isEmpty() || concepto == null || concepto.isEmpty() || hora < 0){
+            return false;
+        }             
+        if(cantidad < 0){
+            return false;
+        }else{
+            saldo += cantidad;
+        }
+        
+        Operacion operacion = new Operacion(fecha, concepto, hora, cantidad);
+        this.operaciones.add(operacion);
+        return true;
+    }
+    
+    public void ajustarInteres(){
+        saldo -= interesAnual*saldo;
+    }
+}
