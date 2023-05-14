ab96f95ced03193e2dc57d17fcfe025331393a52
Smoothy <132601230+SmoothyCoronel@users.noreply.github.com>
2023-05-14 12:54:10 +0200

    Add files via upload

---
+++ Programación/Ejercicios/Fantasma-Invaders/core/src/com/politecnicomalaga/spaceinvaders/Disparo.java
@@ -0,0 +1,15 @@
+
+
+import com.politecnicomalaga.spaceinvaders.ObjetoVolador;
+
+public class Disparo extends ObjetoVolador {
+
+    public Disparo(float pX, float pY, float vY, String imgStr, float w, float h) {
+        super(pX,pY,0f,vY,imgStr,w,h);
+    }
+
+    @Override
+    public void moverse() {
+        posY += velY;
+    }
+}
