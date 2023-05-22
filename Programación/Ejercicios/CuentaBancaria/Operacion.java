d4c4aedc7487f7c204066ada606e1e0f2dc6c511
Smoothy <132601230+SmoothyCoronel@users.noreply.github.com>
2023-05-22 18:52:55 +0200

    Add files via upload

---
+++ Programación/Ejercicios/CuentaBancaria/Operacion.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuentabancaria;
+
+/**
+ *
+ * @author mint
+ */
+public class Operacion {
+    private String fecha, concepto;
+    private byte hora;
+    private float cantidad;
+
+    public Operacion(String fecha, String concepto, byte hora, float cantidad) {
+        this.fecha = fecha;
+        this.concepto = concepto;
+        this.hora = hora;
+        this.cantidad = cantidad;
+    }
+}
